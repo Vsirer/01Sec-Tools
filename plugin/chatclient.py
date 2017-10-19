db5a1dcaa50b6ce92c35a2537bdd42d2a7fa24a6
Sedate0x00 <859009360@qq.com>
2017-10-19 19:12:13 +0800

    01Sec Tools v0.1.1

--- plugin/chatclient.py
+++ plugin/chatclient.py
@@ -14,11 +14,12 @@ my_name = ''
 
 
 def join_server(event, host, port, nickName, chat_result):
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         chat_result.delete(0.0, END)
         sock.connect((host, port))
         sock.send(b'1')
-        print(sock.recv(1024).decode())
+        print(sock.recv(4096).decode())
         # nickName = input('[*] INFO: Input You Name：')
         sock.send(nickName.encode())  # 发送名字
         chat_result.insert(END, '[*] INFO: Login Successful，' + nickName + '\n')
@@ -30,6 +31,12 @@ def join_server(event, host, port, nickName, chat_result):
         chat_result.insert(END, '[*] INFO: Login Failed\n')
         print(e)
 
+def logout_server(event, chat_result):
+    sock.send(b'0')
+    #sock.shutdown(socket.SHUT_RDWR)  #关闭socket所有功能
+    #sock.close()
+
+    chat_result.insert(END, '[*] INFO: logoff current user' + '\n')
 
 def sendThreadFunc(event, chat_result, chat_msg):
     try:
@@ -42,8 +49,9 @@ def sendThreadFunc(event, chat_result, chat_msg):
         chat_msg.delete(0.0, END)
         chat_msg.insert(1.0, '[root@01Sec ~]# ')
     except ConnectionAbortedError:
-        chat_result.insert(END, '[*] WARNING: Server Closed This Connection!\n')
-        print('[*] WARNING: Server Closed This Connection!')
+        pass
+        #chat_result.insert(END, '[*] WARNING: Server Closed This Connection!\n')
+        #print('[*] WARNING: Server Closed This Connection!')
     except ConnectionResetError:
         chat_result.insert(END, '[*] WARNING: Server Is Closed!\n')
         print('[*] WARNING: Server Is Closed!')
@@ -52,30 +60,16 @@ def sendThreadFunc(event, chat_result, chat_msg):
 def recvThreadFunc(chat_result):
     while True:
         try:
-            otherword = sock.recv(1024)
+            otherword = sock.recv(4096)
             if otherword:
                 LocalTime = time.strftime('%H:%M:%S', time.localtime(time.time()))  # 获取本地当前时间
                 chat_result.insert(END, LocalTime + ' ' + otherword.decode() + '\n')
                 print(LocalTime + ' ' + otherword.decode())
             else:
                 pass
-        except ConnectionAbortedError:
-            chat_result.insert(END, '[*] WARNING: Server Closed This Connection!\n')
-            print('[*] WARNING: Server Closed This Connection!')
-        except ConnectionResetError:
-            chat_result.insert(END, '[*] WARNING: Server Is Closed!\n')
-            print('[*] WARNING: Server Is Closed!')
-
-
-'''
-def start_chat(event,host,port,nickName,chat_result):
-
-    th1 = threading.Thread(target=sendThreadFunc)
-    th2 = threading.Thread(target=recvThreadFunc)
-    threads = [th1, th2]
-
-    for t in threads:
-        t.setDaemon(True)
-        t.start()
-    t.join()
-'''
+        except:
+            pass
+            #chat_result.insert(END, '[*] WARNING: Server Closed This Connection!\n')
+            #print('[*] WARNING: Server Closed This Connection!')
+            #sock.close()
+            #return
